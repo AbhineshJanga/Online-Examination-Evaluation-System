2af7bcb00eb6563692f2fcdb15f0bd455e0b6c93
Abhinesh Janga <abhineshjanga77@gmail.com>
2026-03-10 20:53:47 +0530

    Add JWT authentication, user model , and auth APIs

--- src/server.js
+++ src/server.js
@@ -1,19 +1,41 @@
 const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv");
+
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+
+dotenv.config();
 
 const app = express();
+
+// Connect Database
+connectDB();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
-// Test route
+// Routes
+app.use("/api/auth", authRoutes);
+
+// Test Route
 app.get("/", (req, res) => {
-    res.send("Online Examination Backend Running");
+  res.send("Online Examination Backend Running");
+});
+const verifyToken = require("./middleware/authMiddleware");
+
+app.get("/api/protected", verifyToken, (req, res) => {
+
+  res.json({
+    message: "Protected route accessed",
+    user: req.user
+  });
+
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
